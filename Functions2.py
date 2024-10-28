efbbc4bd2f878910a6ecd209f4609d1ccfcf56b8
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-10-28 10:19:37 +0530

    Create Functions2.py

---
+++ Functions2.py
@@ -0,0 +1,5 @@
+# Write a function add(a, b, /) that only allows positional arguments. The function should return the sum of a and b.
+# Write a function greet(*, name) that only accepts name as a keyword argument and returns a greeting message.
+# Write a function describe_person(age, /, *, name) that requires age as a positional-only argument and name as a keyword-only argument. The function should return a description
+# Write a function that accepts any number of positional arguments and returns their product.
+# Write a function that accepts any number of keyword arguments and iterate the List of Numbers By using While Loops.
